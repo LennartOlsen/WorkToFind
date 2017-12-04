577d1c950efe3e01e0002849989a8f53c1b4b107
ltotsas <ltotsas@gmail.com>
2017-12-04 17:17:02 +0100

    notification npm thingy

--- public/src/main.js
+++ public/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
+import Notifications from 'vue-notification'
+
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
+Vue.use(Notifications)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'firebaseui/dist/firebaseui.css'
