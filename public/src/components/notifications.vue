ac1e68e5d93b8891abe105037804013621726100
ltotsas <ltotsas@gmail.com>
2017-12-15 12:19:15 +0100

    a fluffy way to secure only one notification per time but it works

--- public/src/components/notifications.vue
+++ public/src/components/notifications.vue
@@ -11,28 +11,31 @@ export default {
     name : 'notification-component',
     data : function (){
         return {
+            fireTimes : 0,
         }
     },
     mounted : function (){
         ContractStore.SubscribeToNotifications( (addedWinner,contract) =>{
-            this.fireNotification(addedWinner,contract)
+            this.fireNotification(addedWinner, contract,  this.fireTimes += 1)
         })
     },
     methods : {
-        fireNotification(addedWinner, contract){
-            if (addedWinner != null) {
-                if (Settings.getCurrentUser().uid == addedWinner) {
+        fireNotification(addedWinner, contract, fireTimes){
+            if (addedWinner != null && fireTimes == 1 && contract.completedBy == null) {
+                // if (Settings.getCurrentUser().uid == addedWinner) {
+                    console.log("selection")
                     let text = `<a href="/profile/${contract.uid}">The Employer</a> selected your bidding <br>`
                     this.show("success", "success", text, contract)
-                }
+                // }
             }
-            if (Object.keys(contract.completedBy) != null){
+            if (contract.completedBy != null && fireTimes == 1){
+                console.log("complettion")
                 let completedContracts = Object.keys(contract.completedBy)
                 for ( let index in  completedContracts) {
-                    if (Settings.getCurrentUser().uid == completedContracts[index]) {
+                    // if (Settings.getCurrentUser().uid == completedContracts[index]) {
                         let text = `<a href="/profile/${contract.uid}">The Employer</a> marked the task as completed <br>`
                         this.show("success", "success", text, contract)
-                    }
+                    // }
                 }
             }
         },
